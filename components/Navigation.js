a9590aa9a081b53453c553027cc2c4495e7e6f97
Reese Jones <reesealanj@gwu.edu>
2021-03-29 00:17:53 -0400

    Changed navbar

--- components/Navigation.js
+++ components/Navigation.js
@@ -3,16 +3,9 @@ import Link from "next/link";
 export default function Navigation() {
     return (
         <>
-            <nav className="flex items-center flex-wrap bg-grey-600 p-4">
-                <Link href="/">
-                    <a className="inline-flex items-center p-3 mr-4">
-                        <span className="text-xl text-black font-bold tracking-wide">
-                            TKE Virtual Mixer Leaderboard
-                        </span>
-                    </a>
-                </Link>
+            <nav className="flex items-center flex-wrap bg-gray-500 p-4">
                 <div className="hidden w-full lg:inline-flex lg:flex-grow lg:w-auto">
-                    <div className="lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
+                    <div className="lg:inline-flex lg:flex-row lg:mx-auto lg:w-auto w-full lg:items-center items-start  flex flex-col lg:h-auto">
                         <Link href="/">
                             <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-black font-bold items-center justify-center hover:bg-red-500 hover:text-white ">
                                 Leaderboard
