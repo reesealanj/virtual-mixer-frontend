6c136368da6bd9fdfaca06eafce3cc8076d1a66d
Reese Jones <reesealanj@gwu.edu>
2021-03-30 00:01:12 -0400

    postcss edits

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-    purge: [],
+    purge: [
+        "./src/pages/**/*.{js,ts,jsx,tsx}",
+        "./src/components/**/*.{js,ts,jsx,tsx}",
+    ],
     darkMode: false, // or 'media' or 'class'
     theme: {
-        extend: {},
+        extend: {
+            fontFamily: {
+                sans: ["Inter", ...defaultTheme.fontFamily.sans],
+            },
+        },
     },
     variants: {
         extend: {},
     },
-    plugins: [require("@tailwindcss/forms")],
+    // eslint-disable-next-line global-require
+    plugins: [
+        require("@tailwindcss/typography"),
+        require("@tailwindcss/forms"),
+    ],
 };
